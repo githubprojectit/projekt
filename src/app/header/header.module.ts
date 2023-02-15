2996191148f3f5e6f44f47b212131cbe690ef64f
githubprojectit <rafalgithubproject@gmail.com>
2023-02-15 20:39:16 +0100

    init

---
+++ src/app/header/header.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+
+
+@NgModule({
+  exports: [
+    HeaderComponent
+  ],
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule
+  ]
+})
+export class HeaderModule { }
